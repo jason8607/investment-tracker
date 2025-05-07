81cae78470f3594e7fc7415cf917e7c90c67a981
Jason Chen <jason.chen@anyong.com.tw>
2025-05-07 11:38:53 +0800

    優化構建配置，提高部署效率

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
     },
   },
   base: './',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
